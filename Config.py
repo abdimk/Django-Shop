fa999e7c47d9ca55b7ce6e868397fec49103da78
abdimk <abdisamk@gmail.com>
2025-07-30 20:12:10 +0300

    Initial Commit

---
+++ Config.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ['DB_NAME'] = 'mydb'
+os.environ['DB_USER'] = 'myuser'
+os.environ['DB_PASSWORD'] = 'mypassword'
+os.environ['DB_HOST'] = '89.117.37.86'
+os.environ['DB_PORT'] = '5432'
