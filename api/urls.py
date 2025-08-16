1c589133eb20debaa0a44745aa7b709ff15c723c
abdimk <abdisamk@gmail.com>
2025-08-16 15:53:22 +0300

    feat:added class based view for product info

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/',views.ProductListAPIView.as_view(), name="products"),
-    path('products/info/', views.product_info),
+    path('products/info/', views.ProductInfoAPIView.as_view()),
     path('products/<int:pk>/',views.ProductDetailAPIView.as_view(), name="singleProduct"),
     path('orders/', views.OrderListAPIView.as_view()),
     path('user-orders/', views.UserOrderListAPIView.as_view(), name='user_orders')
--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
+from rest_framework.views import APIView
 
 # @api_view(['GET'])
 # def product_list(request):
@@ -69,13 +70,26 @@ class UserOrderListAPIView(generics.ListAPIView):
     
 
 
-@api_view(['GET'])
-def product_info(request):
-    products = models.Product.objects.all()
-    product_info = ProductInfoSerializer({
-        'products':products,
-        'count':len(products),
-        'max_price':products.aggregate(max_price=Max('price'))['max_price']
-    })
-    return Response(product_info.data)
+class ProductInfoAPIView(APIView):
+    def get(self, request):
+        products = models.Product.objects.all()
+        product_info = ProductInfoSerializer({
+            'products':products,
+            'count':len(products),
+            'max_price':products.aggregate(max_price=Max('price'))['max_price']
+        })
+        return Response(product_info.data)
+
+
+
+
+# @api_view(['GET'])
+# def product_info(request):
+#     products = models.Product.objects.all()
+#     product_info = ProductInfoSerializer({
+#         'products':products,
+#         'count':len(products),
+#         'max_price':products.aggregate(max_price=Max('price'))['max_price']
+#     })
+#     return Response(product_info.data)
     
